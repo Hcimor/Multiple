b18c7a9375609aadc377e5fb873f986fbdb1e4e1
Roman Kvateruk <rkvateruk@alfabank.ru>
2023-12-17 21:36:01 +0300

    Убран импорт неиспользуемых библиотек.

--- app/src/main/java/ru/hcimor/multiple/MainActivity.java
+++ app/src/main/java/ru/hcimor/multiple/MainActivity.java
@@ -1,13 +1,9 @@
 package ru.hcimor.multiple;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
